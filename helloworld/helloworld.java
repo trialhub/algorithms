38cd09dac741a9e9d217a3af6c7f4a80e46dabb5
emre <kancaemre@gmail.com>
2014-01-12 17:44:45 +0200

    steriotype hw examlple

---
+++ helloworld/helloworld.java
@@ -0,0 +1,8 @@
+class helloworld {
+	public static void main(String[] args) {
+		System.console().printf(args[0] + "\n");
+		String input = System.console().readLine();// see the parenthesis after console
+		System.out.println(input);
+
+	}
+}
